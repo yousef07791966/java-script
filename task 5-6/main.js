2492a7f938d055c000dfbbe8f325335ab35067f3
yousef07791966 <yu.yousseff@gmail.com>
2024-06-05 17:21:51 +0300

    ..

---
+++ task 5-6/main.js
@@ -0,0 +1,16 @@
+
+
+/////////////////////////////// tasx 1 
+// function upperCase() {
+//     const x = document.getElementById("fname");
+//     x.value = x.value.toUpperCase();
+//   }
+
+
+function mOver(obj) {
+  obj.innerHTML = " hello world !"
+}
+
+function mOut(obj) {
+  obj.innerHTML = " can i help you ? "
+}
